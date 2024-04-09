8ad1ac787c87b61de0535b351df9529fc88bc036
Sem Foudraine <PS238405@summacollege.nl>
2024-04-09 12:10:27 +0200

    Header Gefixt en andere styles toegepast

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,20 +1,39 @@
 <header>
-     <a class="header-img-a" href="/"><img class="header-img" src="img/logo_bg.png" alt="logo"></a>
-     <div id="header-links" class="header-links">
-          <a href="/">Home</a>
-          <a href="menu">Menu</a>
+    <a class="header-img-a" href="/"><img class="header-img" src="img/logo_bg.png" alt="logo"></a>
+    <div id="header-links" class="header-links">
+        <a href="/">Home</a>
+        <a href="menu">Menu</a>
 
-          <div onclick="opencartwinkelwagen()" id="open-cart" class="open-cart">
-               <a style="display: flex;" class="open-cart"><span class="material-symbols-outlined">shopping_cart</span></a>
-               <div id="cartcounter" class="cart-counter">0</div>
-          </div>
-     </div>
-     <div class="header-auth">
-          @if (Route::has('login') && Auth::check())
-          <a href="{{ url('/dashboard') }}">Dashboard</a>
-          @elseif (Route::has('login') && !Auth::check())
-          <a href="{{ url('/login') }}">Login</a>
-          <a href="{{ url('/register') }}">Register</a>
-          @endif
-     </div>
+        <div onclick="opencartwinkelwagen()" id="open-cart" class="open-cart">
+            <a style="display: flex;" class="open-cart"><span class="material-symbols-outlined">shopping_cart</span></a>
+            <div id="cartcounter" class="cart-counter">0</div>
+        </div>
+    </div>
+    <div class="header-auth">
+        @if (Route::has('login') && Auth::check())
+            <a href="{{ url('/dashboard') }}">Dashboard</a>
+        @elseif (Route::has('login') && !Auth::check())
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Register</a>
+        @endif
+    </div>
+
+    <section id="cart-popup" class="shopping-cart">
+        <div class="cart">
+            <h1>Bestelling</h1>
+            <ul id="cart-items"></ul>
+        </div>
+        <div class="contact-info">
+            <h1>Contact Info</h1>
+
+            <button id="closecart" onclick="closecart()">Sluiten</button>
+        </div>
+        <div class="summary">
+            <h1>Summary</h1>
+            <div>
+                <div id="summary-items"></div>
+                <div id="total-price"></div>
+            </div>
+        </div>
+    </section>
 </header>

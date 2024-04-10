7ea9a90aa5b580274f223e40bd2b5d8b53bc3e38
Sem Foudraine <PS238405@summacollege.nl>
2024-04-10 11:53:52 +0200

    Update menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -25,7 +25,7 @@
                             <p class="pizza-price">${{ $pizza->price }}</p>
                             <label for="pizza-quantity">Aantal:</label>
                             <input type="number" class="pizza-quantity" id="pizza-quantity" value="1" min="1" max="99" data-quantity="{{ $pizza->quantity }}">
-                            <button onclick="openpizza(this)" class="order-button" data-pizza="{{ $pizza->name }}">Order Now</button>
+                            <button onclick="openpizza(this)" class="order-button" data-pizza="{{ $pizza->name }}">Bestel Nu</button>
                         </div>
                     </div>
                     @endforeach

89e6284e38738bd5c58f80e61ebecbc670efb469
Sem Foudraine <PS238405@summacollege.nl>
2024-04-12 09:33:11 +0200

    migration gefixt

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('woonplaats')->nullable();
             $table->string('postcode')->nullable();
             $table->string('adres')->nullable();
+            $table->string('huisnummer')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

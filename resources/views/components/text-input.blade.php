8ae6db7b2a2ad5eb2a3eca08841e20b6e189a876
SemFoudraine <PS238405@summacollege.nl>
2024-04-13 10:32:26 +0200

    kleuren aangepast

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-red-600 focus:ring-red-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-stonks-groen focus:border-stonks-groen2 focus:ring-stonks-groen rounded-md shadow-sm']) !!}>

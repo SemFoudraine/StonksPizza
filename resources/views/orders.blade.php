d11964e9fafb63b12f04791f115b0cf5eba9ddeb
Lamajoch <129499114+Lamajoch@users.noreply.github.com>
2024-04-18 10:49:52 +0200

    orders collapse

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -65,14 +65,29 @@
                 </div>
             </div>
             <hr class="my-4 border-gray-300">
-            <h5 class="text-lg font-semibold">Bestelde Items:</h5>
-            <ul class="list-disc pl-6">
+            <h5 class="text-lg font-semibold flex justify-between items-center">
+                <span>Bestelde Items:</span>
+                <button class="focus:outline-none text-gray-500 hover:text-gray-700" onclick="toggleAllOrderItems('orderItems{{ $order->id }}')">
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                    </svg>
+                </button>
+            </h5>
+            <ul id="orderItems{{ $order->id }}" class="list-disc pl-6 hidden">
                 @foreach($order->items as $item)
-                <li>{{ $item->pizza_name }} - {{ $item->quantity }}x</li>
-            @endforeach
-
+                <li id="orderItem{{ $item->id }}" class="order-item">
+                    <span>{{ $item->pizza_name }} - {{ $item->quantity }}x</span>
+                </li>
+                @endforeach
             </ul>
         </div>
     </div>
     @endforeach
 </div>
+
+<script>
+    function toggleAllOrderItems(itemId) {
+        const orderItems = document.getElementById(itemId);
+        orderItems.classList.toggle('hidden');
+    }
+</script>

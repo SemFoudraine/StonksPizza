e862e621d30735cf9a60005ef638d0296917951e
Sem Foudraine <PS238405@summacollege.nl>
2024-04-22 22:55:48 +0200

    nog een kleine ui change

--- resources/views/beheer/bestellingen/orders.blade.php
+++ resources/views/beheer/bestellingen/orders.blade.php
@@ -178,12 +178,12 @@
                             </p>
                         </div>
                     </div>
-                    <hr class="my-5 h border-gray-300 ml-6 mr-6">
+                    <hr class="my-5 border-gray-300 ml-6 mr-6">
                     <h5 class="text-lg font-semibold flex justify-between items-center pb-4 ml-4">
                         <span>Bestelde Items:</span>
-                        <button class="focus:outline-none text-gray-500 hover:text-gray-700"
+                        <button class="focus:outine-none text-gray-500 hover:text-gray-700 mr-5"
                             onclick="toggleAllOrderItems('orderItems{{ $order->id }}')">
-                            <svg class="w-4 h-4 mr-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7">
                                 </path>
